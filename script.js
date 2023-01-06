f70484f37c455e506a9eb3195a6878c1d497177f
Davidgriff98 <davidgriffinemail@gmail.com>
2023-01-05 21:25:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const api_key = '80499a64a51f7b88c3f5ceab77272dde';
 const base_url = 'https://api.themoviedb.org/3';
 const api_url = base_url + '/discover/movie?sort_by=popularity.desc';
 const img_url = 'https://image.tmdb.org/t/p/w500';
+const searchUrl = base_url + '/search/movie?' + api_key;
 
 const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
 
 function getMovies(url){
 
@@ -43,3 +47,13 @@ function showMovies(data) {
 
     })
 }
+
+form.addEventListener('sumbit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if(searchTerm) {
+        getMovies(searchUrl + '&query=' + searchTerm)
+    }
+})
