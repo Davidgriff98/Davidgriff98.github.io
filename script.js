c120ae6161d250382572836096a79873f04bf585
Davidgriff98 <davidgriffinemail@gmail.com>
2023-01-05 20:42:17 -0500

    massive js update

--- script.js
+++ script.js
@@ -4,20 +4,41 @@ const base_url = 'https://api.themoviedb.org/3';
 //const api_url = base_url + 
 const img_url = 'https://image.tmdb.org/t/p/w500';
 
+const main = document.getElementById('main');
+
 function getMovies(url){
 
     fetch(url).then(res => res.json()).then(data => {
-        console.log(data)
+        console.log(data.results)
         showMovies(data.results);
     })
 }
 
 function showMovies(data) {
+    main.innerHTML = '';
 
     data.forEach(movie => {
-        const movieE1 = document.createElement('div');
-        movieE1.classList.add('movie');
+        const {title, poster, overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+        <img src = "${img_url + poster_path }" alt="${title}">
+
+        <div class="movie-info">
+          <h3>${title}</h3> 
+          
+        </div>
+
+        <div class="overview">
+            <h3>"overview"</h3>
+            ${overview}
+
+
+          </div>
         
+        
+        `
+        main.appendChild(movieEl)
 
 
     })
